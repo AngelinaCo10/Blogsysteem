bdba695acb2051b891b799161f789cd07bf2cd37
Angelina <i537166@fontysict.nl>
2026-05-15 14:32:11 +0200

    Next.js sctructure made + Installed ShadCN +Added Card and Data Table components

---
+++ src/app/(visitor)/category/client.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Client() {
+    const [] = useState("da")
+    return <div>client
+    </div>;
+}
---
+++ src/app/(visitor)/category/page.tsx
@@ -0,0 +1,10 @@
+
+import Client from "./client";
+const metadata = {
+    title: "Category",
+}
+export default function Page()
+{
+    
+    return <Client />
+}
