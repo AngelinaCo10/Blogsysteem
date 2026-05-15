b4d52c713028022e73b4db794bfd9da4ea19c795
Angelina <i537166@fontysict.nl>
2026-05-15 14:50:33 +0200

    ShadCN components

---
+++ src/app/(visitor)/blogs/blogoverview/page.tsx
@@ -0,0 +1,9 @@
+import { CardImage } from "@/components/cards/normalCard";
+
+export default function BlogOverviewPage() {
+  return (
+    <main className="p-10">
+      <CardImage />
+    </main>
+  )
+}
