21d3ce8754ae22848d691b80a730e00c9b344ca5
Angelina <i537166@fontysict.nl>
2026-06-02 14:11:18 +0200

    Started blogoverview page

---
+++ src/app/(visitor)/visitor-blogoverview/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+export default function blogoverview() {
+
+
+  return (
+    <div className="container mx-auto w-full max-w-10xl px-4 py-10 lg:px-8">
+       <h1 className="text-3xl font-bold text-[#52525C]">Blogs</h1>
+
+    </div>
+  )
+}
